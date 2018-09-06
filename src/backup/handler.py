f77c78bd9d13c1fbbbad478a8b3dac4d82ec93f6
bbckr <bbckr@users.noreply.github.com>
2018-09-05 21:06:58 -0500

    Deploy infrastructure with Terraform (#8)
    * Use serverless only for packaging
    
    * Rename backup handler file name
    
    * Add terraform
    
    * Update README

--- src/backup/handler.py
+++ src/backup/handler.py
@@ -1,7 +1,7 @@
 import json
 
 
-def handler(event, context):
+def backup(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
