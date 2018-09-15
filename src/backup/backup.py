4badf16b3640248ad75f39adb20331803c8ff954
bckr <bri@bckr.io>
2018-09-15 18:50:57 -0500

    Handle and log exceptions

--- src/backup/backup.py
+++ src/backup/backup.py
@@ -1,21 +1,39 @@
+import logging.config
+import logging
 import json
+import yaml
 import jsonschema
 
 
+def setup_logging(default_path='src/shared/logging.yaml', default_level=logging.INFO):
+    with open(default_path, 'rt') as f:
+        try:
+            config = yaml.safe_load(f.read())
+            logging.config.dictConfig(config)
+        except Exception:
+            logging.basicConfig(level=default_level)
+            logging.error('Failed to setup custom logger, using default', exc_info=True)
+            raise
+
+
+def validate_request(payload, default_schema='src/backup/request.schema', default_format_checker=jsonschema.FormatChecker()):
+    with open(default_schema, 'r') as f:
+        try:
+            schema_data = f.read()
+            schema = json.loads(schema_data)
+            jsonschema.validate(payload, schema, format_checker=default_format_checker)
+        except jsonschema.exceptions.ValidationError:
+            logging.error('JSON request payload failed validation', exc_info=True)
+            raise
+
+
 def handler(event, context):
-
-    with open('src/backup/request.schema', 'r') as f:
-        schema_data = f.read()
-    schema = json.loads(schema_data)
-
     try:
-        jsonschema.validate(event, schema, format_checker=jsonschema.FormatChecker())
+        setup_logging()
+        validate_request(event)
     except jsonschema.exceptions.ValidationError as e:
-        print(e.message)
+        return { 'statusCode': 400, 'body': e.message }
+    except Exception as e:
+        return { 'statusCode': 500, 'body': 'Internal error' }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(event)
-    }
-
-    return response
+    return { 'statusCode': 200, 'body': 'Successfully backed up %s' % event['host'] }
