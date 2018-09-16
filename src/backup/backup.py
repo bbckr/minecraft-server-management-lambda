039d550e5c446b5e2fb17b80a19310cb9b526c71
bbckr <bbckr@users.noreply.github.com>
2018-09-15 20:26:04 -0500

    Handle and log exceptions (#16)
    * Handle and log exceptions
    
    * Fix indentation

--- src/backup/backup.py
+++ src/backup/backup.py
@@ -1,21 +1,50 @@
+import logging.config
+import logging
 import json
+import yaml
 import jsonschema
 
 
-def handler(event, context):
+def setup_logging(default_path='src/shared/logging.yaml', default_level=logging.INFO):
+    with open(default_path, 'rt') as f:
+        try:
+            config = yaml.safe_load(f.read())
+            logging.config.dictConfig(config)
+        except Exception:
+            logging.basicConfig(level=default_level)
+            logging.error('Failed to setup custom logger, using default', exc_info=True)
+            raise
 
-    with open('src/backup/request.schema', 'r') as f:
-        schema_data = f.read()
-    schema = json.loads(schema_data)
 
-    try:
-        jsonschema.validate(event, schema, format_checker=jsonschema.FormatChecker())
-    except jsonschema.exceptions.ValidationError as e:
-        print(e.message)
+def validate_request(
+                    payload, default_schema='src/backup/request.schema',
+                    default_format_checker=jsonschema.FormatChecker()):
+    with open(default_schema, 'r') as f:
+        try:
+            schema_data = f.read()
+            schema = json.loads(schema_data)
+            jsonschema.validate(payload, schema, format_checker=default_format_checker)
+        except jsonschema.exceptions.ValidationError:
+            logging.error('JSON request payload failed validation', exc_info=True)
+            raise
 
+
+def return_response(statusCode, body):
     response = {
-        "statusCode": 200,
-        "body": json.dumps(event)
+        'statusCode': statusCode,
+        'body': body
     }
 
     return response
+
+
+def handler(event, context):
+    try:
+        setup_logging()
+        validate_request(event)
+    except jsonschema.exceptions.ValidationError as e:
+        return return_response(400, e.message)
+    except Exception as e:
+        return return_response(500, 'Internal Error')
+
+    return return_response(200, 'Successfully backed up %s' % event['host'])
